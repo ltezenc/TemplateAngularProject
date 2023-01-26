de8f6f1228ccd11173c496c73cde902fb7230e09
= <=>
2023-01-26 08:53:48 -0500

    filtros en módulo clientes

--- kanakku/src/app/all-modules/customers/customers-list/customer-list.component.ts
+++ kanakku/src/app/all-modules/customers/customers-list/customer-list.component.ts
@@ -133,6 +133,92 @@ export class CustomerListComponent implements OnInit {
     })
   }
 
+  buscarAll(x){
+    let texto = ""; //Para validar por que campo buscar
+    if(x == "nombre")
+    {
+      let nombre =<HTMLInputElement> document.getElementById('bsq_nombre');
+      texto += nombre.value.toLocaleLowerCase()
+    }else
+    if(x == "tarifa"){
+      let tarifa =<HTMLInputElement> document.getElementById('bsq_tarifa');
+      texto += tarifa.value.toLocaleLowerCase()
+    }else{
+      let suministro =<HTMLInputElement> document.getElementById('bsq_suministro');
+      texto += suministro.value.toLocaleLowerCase()
+    }
+    let registros = this.cadena[0] //obtengo todo el registro
+    let resultado =  <HTMLInputElement> document.querySelector('#resultados_busqueda');
+    let html = "";
+    let ocultar_tabla =<HTMLInputElement> document.querySelector('#data-table'); //Tabla con registro
+    ocultar_tabla.style.display = "none"; //Ocultar Tabla con registro
+    let mostrar_tabla =<HTMLInputElement> document.querySelector('#table-data'); //Tabla para mostrar los registros de busqueda
+    mostrar_tabla.style.display = "inline-table"; //Mostrar Tabla para  los registros de busqueda
+    for(let registro of registros)
+    {
+      let buscar = "";
+
+      if(x == "nombre")
+        {
+          buscar += registro.razonSocial.toLocaleLowerCase();
+        }else
+        if(x == "tarifa"){
+          buscar += registro.prefijotarifa.toLocaleLowerCase();
+        }else{
+          buscar += registro.suministro.toLocaleLowerCase();
+        }
+      if(buscar.indexOf(texto) !== -1){ //lo encontró
+        html += `
+        <tr>
+          <td>${ registro.id }</td>
+          <td>${ registro.razonSocial }</td>
+          <td>${ registro.prefijotarifa }</td>
+          <td>${ registro.potenciaContratada }</td>`
+          if(registro.suministro == null){
+            html += `
+            <td>
+              <span class="badge badge-pill bg-danger-light" (click)="tempId = ${registro.id}" data-bs-toggle="modal" data-bs-target="#suministroModal"
+                (click)="logMessageId(messageEl)">
+                --
+              </span>
+            </td>`
+          }else{
+            html += `
+                <td>
+                ${ registro.suministro }
+                </td>`
+          }
+          if(registro.estadoId == 1)
+          {
+            html += `
+            <td>
+              <span class="badge badge-pill bg-success-light">Activo</span>
+            </td>
+            `
+          }else{
+            html += `
+            <td>
+              <span class="badge badge-pill bg-danger-light">Activo</span>
+            </td>`
+          }
+      html += `
+      <td class="text-end">
+        <a href="/customers/edit-customer?id=${registro.id}"
+          class="btn btn-sm btn-white text-success me-2"><i class="far fa-edit me-1"></i> Edit</a>
+        <a (click)="tempId = ${registro.id}" data-bs-toggle="modal" data-bs-target="#delete_customer"
+          class="btn btn-sm btn-white text-danger me-2"><i class="far fa-trash-alt me-1"></i>Delete</a>
+      </td>
+          `
+          resultado.innerHTML = html;
+      }
+      if(texto === ""){
+        ocultar_tabla.style.display = "inline-table";
+        resultado.innerHTML = "";
+        mostrar_tabla.style.display = "none";
+      }
+    }
+  }
+
   filter() {}
   deleteCustomer() {
   /*  this.srvModuleService.delete(this.tempId, this.url).subscribe((data) => {
